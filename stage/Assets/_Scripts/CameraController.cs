6fff9fe3a922ebf9efb497a6ba0a6a31deb7ed37
amirensit <amirensit@gmail.com>
2017-06-02 13:55:01 +0200

    semaine 1

---
+++ stage/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset,r;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+            r = Vector3.zero;
+
+
+        else if (Input.GetKeyDown(KeyCode.Q))
+            r = offset;
+
+            transform.position = player.transform.position + r;
+    }
+}
